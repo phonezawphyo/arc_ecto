080eb813b0c0d4a4020a012b31c1b689fa7e109f
Phone Zaw Phyo <phonezawphyo@gmail.com>
2018-06-15 14:48:55 +0000

    [fix]arc version

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Arc.Ecto.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:arc,  "~> 0.8.0"},
+      {:arc, git: "https://github.com/phonezawphyo/arc.git", ref: "master"},
       {:ecto, "~> 2.1"},
       {:mock, "~> 0.1.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev}
